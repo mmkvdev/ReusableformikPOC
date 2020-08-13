3e25185dc3610dc49404b57302578887023de77c
Madhu Kiran Varma <madhuv299@gmail.com>
2020-08-13 16:43:02 +0530

    Update: Select reusable options

--- src/utils/formikUtils.js
+++ src/utils/formikUtils.js
@@ -2,14 +2,23 @@ import * as Yup from 'yup';
 
 export const initialValues = {
     email: '',
-    description: ''
+    description: '',
+    selectOption: ''
 };
 
 export const validationSchema = Yup.object({
     email: Yup.string().required('Required'),
-    description: Yup.string().required('Required')
+    description: Yup.string().required('Required'),
+    selectOption: Yup.string().required('Required')
 });
 
 export const onSubmit = values => {
     console.log(values);
-}
+};
+
+export const dropDownOptions = [
+    { key: 'Select an Option', value: '' },
+    { key: 'Option 1', value: 'option1' },
+    { key: 'Option 2', value: 'option2' },
+    { key: 'Option 3', value: 'option3' }
+];
