8c5b419a6cf3b8533bd77d2d29d7761f3288481c
Madhu Kiran Varma <madhuv299@gmail.com>
2020-08-13 14:45:47 +0530

    Update - Formik Controller - Input

--- src/components/FormikController.js
+++ src/components/FormikController.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Input from './Input';
 
 // decides which of the different form fields have to be rendered based on one particular props - control
 function FormikController(props) {
-    const { control } = props;
+    const { control, ...rest } = props;
 
     switch (control) {
         case 'input':
+            return <Input {...rest} />
         case 'textarea':
         case 'select':
         case 'radio':
