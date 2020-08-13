b868902889ba9a22a11ee65d44388b4344dca09d
Madhu Kiran Varma <madhuv299@gmail.com>
2020-08-13 14:14:11 +0530

    Update: Starter Files 🏁

---
+++ src/components/FormikContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import { initialValues, validationSchema, onSubmit } from './utils/formikUtils';
+
+function FormikContainer() {
+    return (
+        <div>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit} 
+            >
+                {
+                    (formikProps) => <Form>
+                        <submit type='submit'>Submit</submit>
+                    </Form>
+                }
+            </Formik>
+        </div>
+    )
+}
+
+export default FormikContainer
---
+++ src/components/utils/formikUtils.js
@@ -0,0 +1,9 @@
+import * as Yup from 'yup';
+
+export const initialValues = {};
+
+export const validationSchema = Yup.object({});
+
+export const onSubmit = values => {
+    console.log(values);
+}
