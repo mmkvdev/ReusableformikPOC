848730d9dfd879ceb5377831183d6e7207bbca37
Madhu Kiran Varma <madhuv299@gmail.com>
2020-08-13 16:40:50 +0530

    Update: Formik Container - Select

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
-import { initialValues, validationSchema, onSubmit } from '../utils/formikUtils';
+import { initialValues, validationSchema, onSubmit, dropDownOptions } from '../utils/formikUtils';
 import FormikController from './FormikController';
 
 function FormikContainer() {
@@ -14,17 +14,24 @@ function FormikContainer() {
                 {
                     (formikProps) =>
                         <Form>
-                            <FormikController 
-                                control='input' 
-                                type='email' 
-                                label='Email' 
-                                name='email' 
+                            <FormikController
+                                control='input'
+                                type='email'
+                                label='Email'
+                                name='email'
                             />
 
-                            <FormikController 
+                            <FormikController
                                 control='textarea'
                                 label='Description'
                                 name='description'
+                            />
+
+                            <FormikController
+                                control='select'
+                                label='Select a Topic'
+                                name='selectOption'
+                                options={dropDownOptions}
                             />
                             <button type='submit'>Submit</button>
                         </Form>
