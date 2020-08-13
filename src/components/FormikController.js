5eb803f361263e1a117e9e6b8f0f1630667f9c87
Madhu Kiran Varma <madhuv299@gmail.com>
2020-08-13 16:41:35 +0530

    Update: Formik Controller - Select

--- src/components/FormikController.js
+++ src/components/FormikController.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Input from './Input';
 import TextArea from './TextArea';
+import Select from './Select';
 
 // decides which of the different form fields have to be rendered based on one particular props - control
 function FormikController(props) {
@@ -12,6 +13,7 @@ function FormikController(props) {
         case 'textarea':
             return <TextArea {...rest} />
         case 'select':
+            return <Select {...rest} />
         case 'radio':
         case 'checkbox':
         case 'date':
