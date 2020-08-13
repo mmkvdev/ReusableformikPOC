3af0d3c55c86196ce210e002bffb243483144686
Madhu Kiran Varma <madhuv299@gmail.com>
2020-08-13 15:58:05 +0530

    Update: TextArea Utils

--- src/utils/formikUtils.js
+++ src/utils/formikUtils.js
@@ -1,8 +1,14 @@
 import * as Yup from 'yup';
 
-export const initialValues = {};
+export const initialValues = {
+    email: '',
+    description: ''
+};
 
-export const validationSchema = Yup.object({});
+export const validationSchema = Yup.object({
+    email: Yup.string().required('Required'),
+    description: Yup.string().required('Required')
+});
 
 export const onSubmit = values => {
     console.log(values);
