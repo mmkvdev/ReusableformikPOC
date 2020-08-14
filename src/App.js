aea871722f03094a29bc2f0303522cbcd295713f
mmkvdev <madhuv299@gmail.com>
2020-08-14 17:26:03 +0530

    Update: Course Enrollment Form Scripts and Utils

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 // import FormikContainer from './components/FormikContainer';
 // import LoginForm from './components/LoginForm';
 import RegistrationForm from './components/RegistrationForm';
+import CourseEnrollmentForm from './components/CourseEnrollmentForm';
 
 function App() {
   return (
     <div className="App">
-      <RegistrationForm />
+      <CourseEnrollmentForm />
     </div>
   );
 }
